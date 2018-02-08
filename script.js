de682e4698820399d6770afaf9108ef777c7c6a5
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-08 23:54:29 +0100

    console.log(john.lastName);

--- script.js
+++ script.js
@@ -13,15 +13,29 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function() {
-        console.log(2016 - this.yearOfBirth);
-    }
 }
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+}
+
+// We can also add properties instead of methods likes above, but it's not really commom.
+// The following is the example
+Person.prototype.lastName = 'Smith';
 
 // This is how we create new objects, using the contruction function
 // This is called instantiation, because the objects here are instances of the person object
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
 
 john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
 
 
