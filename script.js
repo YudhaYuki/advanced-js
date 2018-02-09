bec2d05a890681cf825ddbfab1aff4c9023ee43c
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-09 13:15:19 +0100

    Object.create PART I

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 ////////////////////////-------Function constructor--------/////////////////////////////
 
 
@@ -36,6 +38,22 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
+*/
+
+
+////////////////////------------ Object.create ------------------/////////////////
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth)
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
 
 
