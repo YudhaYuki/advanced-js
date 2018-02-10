cfba522fb415cd0bd9099c4b66ae961e0f242396
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-10 23:54:21 +0100

    CHALLANGE I Part I, No 8 Part I

--- script.js
+++ script.js
@@ -360,6 +360,7 @@ c) correct answer (I would use a number for this)
 })();
 */
 
+/*
 (function(){
     // 1
     function Question(question, answers, correct) {
@@ -405,7 +406,7 @@ c) correct answer (I would use a number for this)
     // Call the correctAnswer method
     questions[n].checkAnswer(answer);
 })();
-
+*/
 
 
 /*
@@ -419,4 +420,65 @@ c) correct answer (I would use a number for this)
 
 11. Display the score in the console. Use yet another method for this.
 */
+
+
+(function(){
+    // 1
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    };
+
+    // 4
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    // Check if the answer is correct
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer, try again!');
+        }
+    }
+
+    // 2
+    var q1 = new Question('Is Javascript the coolest programming language i the world?', ['Yes', 'No'], 0);
+    var q2 = new Question('what is the name of this course\' teacher?', ['john', 'Michael', 'Jonas'], 2);
+    var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
+
+
+
+    var questions = [q1, q2, q3];
+
+    // 8
+    function nextQuestion() {
+    
+        var n = Math.floor(Math.random() * questions.length);
+    
+        questions[n].displayQuestion();
+    
+        // parsInt converts string to an integernumber
+        var answer = parseInt(prompt('Please select the correct answer.'));
+    
+        // Call the correctAnswer method
+        questions[n].checkAnswer(answer);
+
+        if(answer !== 'exit') {
+            questions[n].checkAnswer(answer);
+            
+            nextQuestion();   
+        }     
+    }
+
+    nextQuestion();
+
+})();
+
 
