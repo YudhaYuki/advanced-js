eec614e4e609ed3745bc9d85ab343d56c39f3934
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-11 10:49:24 +0100

    --- Expert level --- CHALLANGE

--- script.js
+++ script.js
@@ -440,13 +440,28 @@ c) correct answer (I would use a number for this)
     }
 
     // Check if the answer is correct
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+
         if (ans === this.correct) {
             console.log('Correct answer!');
+            sc = callback(true);
+
         } else {
             console.log('Wrong answer, try again!');
+
+            sc = callback(false);
         }
+
+        this.displayScore(sc);
     }
+
+    // Method to display the score
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score: ' + score);
+        console.log('------------------------------------------------------');
+    }
+
 
     // 2
     var q1 = new Question('Is Javascript the coolest programming language i the world?', ['Yes', 'No'], 0);
@@ -456,6 +471,21 @@ c) correct answer (I would use a number for this)
 
 
     var questions = [q1, q2, q3];
+
+    // 10 functions return function
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            } 
+            return sc;
+        }
+    } 
+
+    var keepScore = score();
+
+
 
     // 8
     function nextQuestion() {
@@ -467,10 +497,11 @@ c) correct answer (I would use a number for this)
         var answer = prompt('Please select the correct answer.');
     
         // Call the correctAnswer method
-        questions[n].checkAnswer(parseInt(answer));
+        // questions[n].checkAnswer(parseInt(answer));
 
+        // 9
         if(answer !== 'exit') {
-            questions[n].checkAnswer(answer);
+            questions[n].checkAnswer(parseInt(answer), keepScore);
 
             nextQuestion();   
         }     
