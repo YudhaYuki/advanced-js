d8b93226f95a61018224f3c776399af0f48d47e7
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-15 10:43:19 +0100

    PRIMITIVES vs OBJECTS

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -33,7 +33,7 @@ console.log(mark.lastName);
 
 
 ///////////////////// ---- OBJECT CREATE ---- ////////////////////
-
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -55,12 +55,48 @@ var jane = Object.create(personProto,
 
 console.log(jane);
 console.log(john);
+*/
 
 
 
 
+///////////////////// ---- PRIMITIVES vs OBJECTS---- ////////////////////
+
+// Primitive
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
 
 
+// Object
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Function
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Fransisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city)
 
 
 
