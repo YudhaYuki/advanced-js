0ac3be97e6c77e1222a9d1c447c74ebaf3be3b9a
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-15 17:00:18 +0100

    Challange step 10 - 11

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -422,12 +422,24 @@ c) correct answer (I would use a number for this)
             }
         }
     
-        Question.prototype.checkAnswer = function(ans) {
+        Question.prototype.checkAnswer = function(ans, callback) {
+            var sc;
+
             if (ans === this.correct) {
                 console.log('Correct answer!');
+                sc = callback(true);
             } else {
                 console.log('Wrong answer, try again!');
+
+                sc = callback(false);
             }
+
+            this.displayScore(sc);
+        }
+
+        Question.prototype.displayScore = function(score) {
+            console.log('Your current score is ' + score);
+            console.log('-----------------------------------------------');
         }
     
         var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['YES', 'NO'], 0);
@@ -438,8 +450,20 @@ c) correct answer (I would use a number for this)
 
 
 
-        var questions = [q1, q2, q3];        
+        var questions = [q1, q2, q3];       
+        
+        function score() {
+            var sc = 0;
+            return function(correct) {
+                if (correct) {
+                    sc++;
+                } 
+                return sc;
+            }
+        }
     
+        var keepScore = score();
+        
         function nextQuestion() {
             
             var n = Math.floor(Math.random() * questions.length);
@@ -451,7 +475,7 @@ c) correct answer (I would use a number for this)
 
             if (answer !== 'exit') {
 
-                questions[n].checkAnswer(parseInt(answer));           
+                questions[n].checkAnswer(parseInt(answer), keepScore);           
 
                 nextQuestion();                
             }
